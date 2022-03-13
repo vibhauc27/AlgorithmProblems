db1abad80b5f891c77c7bf7c740fb8f627439d8f
Vibhauc_27 <ucvibha@gmail.com>
2022-03-13 15:54:00 -0700

    [Vibha]Added:Bubble Sort Program

---
+++ AlgorithmProblems/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace BubbleSort
+{
+    class MySort
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5,2,6,9,1 };
+            int temp;
+
+            Console.WriteLine("The original elements are:");
+            foreach (int a in array)
+            Console.Write(a + " ");
+
+            for (int j = 0; j <= array.Length - 2; j++)
+            {
+                for (int i = 0; i <= array.Length - 2; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("The bubble sorted elements are:");
+            foreach (int p in array)
+            Console.Write(p + " ");
+           
+        }
+    }
+}
